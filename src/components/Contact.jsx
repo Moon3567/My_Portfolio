3b2887477230fb2a82e26115835c5a8130e9a498
Moon3567 <Mustafaali3568@gmail.com>
2023-05-02 18:13:18 +0100

    contact page update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -27,8 +27,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        "service_sa91zrc",
-        "template_5ed4xen",
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
           to_name: "Mustafa",
@@ -36,7 +36,7 @@ const Contact = () => {
           to_email: "mustafaali3568@gmail.com",
           message: form.message,
         },
-        "pRjXY1jMO0NpAIyq_"
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
